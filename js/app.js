29719faecc04eaa16ba5627d616a1419b6b92088
Alex Basystyi <alexbasisty@gmail.com>
2020-04-10 12:30:24 +0200

    created field with div's

--- js/app.js
+++ js/app.js
@@ -3,8 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     constructor(width, height) {
       this.boardWidth = width;
       this.boardHeight = height;
+      this.board = document.getElementById('board');
+    }
+    createBoard = () => {
+      this.board.style.width = `${this.boardWidth * 10}px`;
+      this.board.style.height = `${this.boardHeight * 10}px`;
+      const fieldsSum = this.boardHeight * this.boardWidth;
+      for(let i = 0; i < fieldsSum; i++) {
+        const newDiv = document.createElement('div');
+        this.board.appendChild(newDiv);
+      }
     }
   }
-  const game = new GameOfLife(10,10)
-  console.log(game.boardHeight, game.boardWidth);
+  const game = new GameOfLife(10,10);
+  game.createBoard();
 })
