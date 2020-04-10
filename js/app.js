d349499aab8d158d335b20fd4d88ec053ace8098
Alex Basystyi <alexbasisty@gmail.com>
2020-04-10 12:08:32 +0200

    class Game of Life

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('hello');
-
+  class GameOfLife {
+    constructor(width, height) {
+      this.boardWidth = width;
+      this.boardHeight = height;
+    }
+  }
+  const game = new GameOfLife(10,10)
+  console.log(game.boardHeight, game.boardWidth);
 })
