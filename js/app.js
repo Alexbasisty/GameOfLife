ca2adb16178bb87ce91589977c6e4fc6eeb31a9c
Alex Basystyi <alexbasisty@gmail.com>
2020-04-11 12:55:26 +0200

    className 'live' toggling

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
       this.boardWidth = width;
       this.boardHeight = height;
       this.board = document.getElementById('board');
+      this.cells = []
     }
     createBoard = () => {
       this.board.style.width = `${this.boardWidth * 10}px`;
@@ -12,7 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
       for(let i = 0; i < fieldsSum; i++) {
         const newDiv = document.createElement('div');
         this.board.appendChild(newDiv);
+        this.cells.push(newDiv);
       }
+
+      this.cells.forEach(cell => {
+        cell.addEventListener('click', event => {
+          event.target.classList.toggle('live')
+        })
+      })
     }
   }
   const game = new GameOfLife(10,10);
