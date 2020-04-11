b93fe4d21b6077823734a402a45a80093d9e4a38
Alex Basystyi <alexbasisty@gmail.com>
2020-04-11 13:44:10 +0200

    computeCellNextState method

--- js/app.js
+++ js/app.js
@@ -28,8 +28,47 @@ document.addEventListener('DOMContentLoaded', () => {
       const index = (x - 1) + (y - 1) * this.boardWidth;
       return this.cells[index];
     }
+
+    setCellState = (x, y, state) => {
+      this.getCoordinates(x, y).classList.add(state)
+    }
+
+    firstGlider = () => {
+      this.setCellState(1,2, 'live')
+      this.setCellState(2,2, 'live')
+      this.setCellState(1,3, 'live')
+      this.setCellState(2,3, 'live')
+      this.setCellState(2,1, 'live')
+    }
+
+    computeCellNextState = (x, y) => {
+      const div = this.getCoordinates(x, y);
+      const neighbour1 = this.getCoordinates(x - 1, y - 1);
+      const neighbour2 = this.getCoordinates(x, y - 1);
+      const neighbour3 = this.getCoordinates(x + 1, y - 1);
+      const neighbour4 = this.getCoordinates(x - 1, y);
+      const neighbour5 = this.getCoordinates(x + 1, y);
+      const neighbour6 = this.getCoordinates(x -1, y + 1);
+      const neighbour7 = this.getCoordinates(x, y + 1);
+      const neighbour8 = this.getCoordinates(x + 1, y + 1);
+      let liveCells = 0;
+      if (neighbour1 !== undefined && neighbour1.className === 'live') {liveCells ++}
+      if (neighbour2 !== undefined && neighbour2.className === 'live') {liveCells ++}
+      if (neighbour3 !== undefined && neighbour3.className === 'live') {liveCells ++}
+      if (neighbour4 !== undefined && neighbour4.className === 'live') {liveCells ++}
+      if (neighbour5 !== undefined && neighbour5.className === 'live') {liveCells ++}
+      if (neighbour6 !== undefined && neighbour6.className === 'live') {liveCells ++}
+      if (neighbour7 !== undefined && neighbour7.className === 'live') {liveCells ++}
+      if (neighbour8 !== undefined && neighbour8.className === 'live') {liveCells ++}
+      if (liveCells < 2 || liveCells > 3) {
+        return 0
+      } else {
+        return 1
+      }
+    }
   }
 
   const game = new GameOfLife(10,10);
   game.createBoard();
+  game.firstGlider();
 })
